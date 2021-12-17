8f79841c9a45ea4f4060c32e904196e074fbefda
Sridhar Ratnakumar <srid@srid.ca>
2021-12-17 17:45:45 -0500

    builtin

--- src/Plut/Sample/Validator/Plutarch.hs
+++ src/Plut/Sample/Validator/Plutarch.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE PostfixOperators #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Plut.Sample.Validator.Plutarch (plutarchValidator) where
 
+import Data.Kind (Type)
 import Data.Nat (Nat (S, Z), nat0, nat1, nat2)
 import Data.Text (Text)
 import Ledger.Scripts (Validator (..))
@@ -24,7 +27,9 @@ plutarchValidator :: Validator
 plutarchValidator =
   Validator $ compile validator
 
--- | TODO: Rewrite raw 'Untyped Plutarch' to use the types from typed eDSL
+{- | TODO: Rewrite raw 'Untyped Plutarch' to use the types from typed eDSL
+ Upstream the types to Plutarch
+-}
 validator :: forall s. Term s (PByteString :--> PByteString :--> PByteString :--> PUnit)
 validator =
   plam $ \datum (_redeemer :: Term s PByteString) (ctx :: Term s PByteString) ->
@@ -32,13 +37,15 @@ validator =
       (pTrace "yoyo:atProduct" $ atProduct 0 $ pTrace "yoyo:ctx" ctx)
       $ \txInfo ->
         plet (atProduct 7 $ pTrace "yoyo:txInfo" txInfo) $ \signatories' ->
-          plet (pUnListData £ punsafeCoerce (pTrace "yoyo:sig" signatories')) $ \signatories ->
+          plet ((PLC.UnListData #) £ punsafeCoerce (pTrace "yoyo:sig" signatories')) $ \signatories ->
             plet (pTrace "yoyo:hasElem" $ hasElem (punsafeCoerce datum) signatories) $ \(isBeneficiary :: Term s PBool) ->
               pif isBeneficiary (pcon PUnit) $ pTrace "yoyo:not-beneficiary" perror
 
 instance PEq POpaque where
   a £== b =
-    pBuiltin PLC.EqualsData nat0 £ a £ b
+    pBuiltin PLC.EqualsData £ a £ b
+
+-- List functions
 
 hasElem :: forall s. Term s POpaque -> Term s POpaque -> Term s PBool
 hasElem k list' =
@@ -58,7 +65,7 @@ hasElem k list' =
 
 atProduct :: Term s PInteger -> Term s a -> Term s POpaque
 atProduct n dat =
-  plet (pTrace "yoyo:pUnConstrData" $ pUnConstrData £ punsafeCoerce dat) $ \dat' ->
+  plet (pTrace "yoyo:pUnConstrData" $ (PLC.UnConstrData #) £ punsafeCoerce dat) $ \dat' ->
     pmatch' (pTrace "yoyo:dat'" dat') $ \(PPair _ products :: PPair s) ->
       atIndex n products
 
@@ -79,44 +86,18 @@ atIndex n =
                 x
                 (self £ (n' - 1) £ xs)
 
--- Returns a pair of Integer and [Data]
-pUnConstrData :: Term s (POpaque :--> POpaque)
-pUnConstrData = pBuiltin PLC.UnConstrData nat0
-
-pUnListData :: Term s (POpaque :--> POpaque)
-pUnListData = pBuiltin PLC.UnListData nat0
-
-pBuiltin :: PLC.DefaultFun -> Nat -> Term s a
-pBuiltin builtin forceLevel =
-  phoistAcyclic $ forceN forceLevel $ punsafeBuiltin builtin
-  where
-    forceN :: Nat -> Term s a -> Term s a
-    forceN Z = id
-    forceN (S n) = pforce . punsafeCoerce . forceN n
-
 -- All of these are working with `Data`
 
--- This is all opaque now, but we want to make it typed.
 data PPair s = PPair (Term s POpaque) (Term s POpaque)
 
 instance PlutusType PPair where
   type PInner PPair _ = POpaque
-  pcon' (PPair a b) = pMkPairData £ a £ b
+  pcon' (PPair a b) = (PLC.MkPairData #) £ a £ b -- There is no MkPair, plutus build constructs pair of data
   pmatch' pair f =
     -- TODO: use delay/force to avoid evaluating `pair` twice?
-    plet (pFstPair £ pair) $ \a ->
-      plet (pSndPair £ pair) $ \b ->
+    plet ((PLC.FstPair #) £ pair) $ \a ->
+      plet ((PLC.SndPair #) £ pair) $ \b ->
         f $ PPair a b
-
-pMkPairData :: Term s (POpaque :--> POpaque :--> POpaque)
-pMkPairData =
-  pBuiltin PLC.MkPairData nat0
-
-pFstPair :: Term s (POpaque :--> POpaque)
-pFstPair = pBuiltin PLC.FstPair nat2
-
-pSndPair :: Term s (POpaque :--> POpaque)
-pSndPair = pBuiltin PLC.SndPair nat2
 
 data PList s
   = PNil
@@ -125,30 +106,52 @@ data PList s
 instance PlutusType PList where
   type PInner PList _ = POpaque
   pcon' PNil = undefined -- TODO??
-  pcon' (PCons x xs) = pMkCons £ x £ xs
+  pcon' (PCons x xs) = (PLC.MkCons #) £ x £ xs
   pmatch' list f =
-    plet (pNullList £ list) $ \isEmpty ->
+    plet ((PLC.NullList #) £ list) $ \isEmpty ->
       pif
         (punsafeCoerce isEmpty)
         (f PNil)
         $ plet
-          (pHeadList £ list)
+          ((PLC.HeadList #) £ list)
           ( \head ->
-              plet (pTailList £ list) $ \tail ->
+              plet ((PLC.TailList #) £ list) $ \tail ->
                 f $ PCons head tail
           )
 
-pMkCons :: Term s (POpaque :--> POpaque :--> POpaque)
-pMkCons = pBuiltin PLC.MkCons nat1
-
-pNullList :: Term s (POpaque :--> PBool)
-pNullList = pBuiltin PLC.NullList nat1
-
-pHeadList :: Term s (POpaque :--> POpaque)
-pHeadList = pBuiltin PLC.HeadList nat1
-
-pTailList :: Term s (POpaque :--> POpaque)
-pTailList = pBuiltin PLC.TailList nat1
+-- Trace
 
 pTrace :: forall a s. Text -> Term s a -> Term s a
-pTrace s f = pBuiltin PLC.Trace nat1 £ pfromText s £ f
+pTrace s f = pBuiltin PLC.Trace £ pfromText s £ f
+
+-- Builtins
+
+(#) :: forall k (s :: k) (a :: k -> Type). PLC.DefaultFun -> Term s a
+(#) = pBuiltin
+infixl 6 #
+
+pBuiltin :: PLC.DefaultFun -> Term s a
+pBuiltin builtin =
+  phoistAcyclic $ forceN (forceLevel builtin) $ punsafeBuiltin builtin
+  where
+    forceN :: Nat -> Term s a -> Term s a
+    forceN Z = id
+    forceN (S n) = pforce . punsafeCoerce . forceN n
+
+class ForceLevel a where
+  forceLevel :: a -> Nat
+
+instance ForceLevel PLC.DefaultFun where
+  forceLevel = \case
+    PLC.EqualsData -> nat0
+    PLC.UnConstrData -> nat0
+    PLC.UnListData -> nat0
+    PLC.MkPairData -> nat0
+    PLC.FstPair -> nat2
+    PLC.SndPair -> nat2
+    PLC.MkCons -> nat1
+    PLC.NullList -> nat1
+    PLC.HeadList -> nat1
+    PLC.TailList -> nat1
+    PLC.Trace -> nat1
+    _ -> undefined
