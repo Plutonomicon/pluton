2db1a017bd81068e9b4ee86143c5d784747ab18b
Sridhar Ratnakumar <srid@srid.ca>
2021-12-22 14:14:12 -0500

    Revert flake.nix to make CI run fast

--- flake.nix
+++ flake.nix
@@ -39,15 +39,18 @@
 
             nativeBuildInputs = [
               pkgs.nixpkgs-fmt
-              pkgs.ormolu
-              pkgs.haskellPackages.cabal-fmt
-              pkgs.hlint
-              pkgs.ghcid
+              # pkgs.ormolu
+              # pkgs.haskellPackages.cabal-fmt
+              # pkgs.hlint
+              # pkgs.ghcid
             ];
 
             tools = {
               cabal = { };
+              ghcid = { };
+              hlint = { };
               fourmolu = { };
+              cabal-fmt = { };
               haskell-language-server = { }; # Must use haskell.nix, because the compiler version should match
             };
 
