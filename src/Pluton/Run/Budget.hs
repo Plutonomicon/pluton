5fae6e1105acfa47a9502cb301a698b23f55806d
Sridhar Ratnakumar <srid@srid.ca>
2021-12-24 16:01:07 -0500

    Add Budget type

--- src/Pluton/Run/Budget.hs
+++ src/Pluton/Run/Budget.hs
@@ -1,6 +1,7 @@
 -- | Execution budget and script size for Plutus scripts
 module Pluton.Run.Budget
-  ( -- | * Budget for an arbitraty Plutus script
+  ( Budget (..),
+    -- | * Budget for an arbitraty Plutus script
     scriptBudget,
     -- | * Budget for a smart contract validator script
     validatorBudget,
@@ -19,7 +20,6 @@ import Data.ByteString.Short qualified as SBS
 import Data.Default (Default (def))
 import Data.Int (Int64)
 import Data.Maybe (fromJust)
-import Data.Monoid (Sum (..))
 import Flat (flat)
 import GHC.Stack (HasCallStack)
 import Ledger.Index (ExBudget, ScriptValidationEvent (..), ValidatorMode (FullyAppliedValidators), getScript)
@@ -30,13 +30,19 @@ import Streaming.Prelude qualified as S
 import Wallet.Emulator.Folds qualified as Folds
 import Wallet.Emulator.Stream (foldEmulatorStreamM)
 
+data Budget = Budget
+  { exBudget :: ExBudget,
+    scriptSizeBytes :: Int64
+  }
+  deriving stock (Show)
+
 -- | Return the exbudget and script size of the *applied* validator run inside
 -- an Emulator trace.
 --
 -- The trace must have run the validation exactly once, else this will fail. We
 -- do this, because we are benchmarking a single run of the validator, not
 -- multiple runs.
-emulatorTraceBudget :: Em.EmulatorTrace a -> (ExBudget, Sum Int64)
+emulatorTraceBudget :: Em.EmulatorTrace a -> Budget
 emulatorTraceBudget trace =
   -- Most of the code here is taken from `Plutus.Trace.Emulator.Extract` (IOHK
   -- doesn't care to export it).
@@ -50,7 +56,7 @@ emulatorTraceBudget trace =
         let bytes = BSL.fromStrict . flat . unScript . getScript mode $ event
             byteSize = BSL.length bytes
             exBudget = either (error . show) fst sveResult
-         in (exBudget, Sum byteSize)
+         in Budget exBudget byteSize
    in f . exactlyOne $ getEvents Folds.scriptEvents
   where
     exactlyOne :: [a] -> a
@@ -61,11 +67,11 @@ emulatorTraceBudget trace =
 --
 -- NOTE: This calculates the budget for an *applied* validator. Use
 -- @emulatorTraceBudget@ if you want to calculate on applied validators.
-validatorBudget :: Validator -> (Plutus.ExBudget, Int)
-validatorBudget = (evalScriptCounting &&& SBS.length) . serialiseValidator
+validatorBudget :: Validator -> Budget
+validatorBudget = uncurry Budget . (evalScriptCounting &&& (fromInteger . toInteger . SBS.length)) . serialiseValidator
 
-scriptBudget :: Script -> (Plutus.ExBudget, Int)
-scriptBudget = (evalScriptCounting &&& SBS.length) . serialiseScript
+scriptBudget :: Script -> Budget
+scriptBudget = uncurry Budget . (evalScriptCounting &&& (fromInteger . toInteger . SBS.length)) . serialiseScript
 
 serialiseScript :: Script -> SBS.ShortByteString
 serialiseScript = SBS.toShort . LB.toStrict . serialise
